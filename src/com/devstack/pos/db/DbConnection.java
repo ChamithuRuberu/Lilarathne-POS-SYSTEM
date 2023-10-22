3a2e80a6f08ba373b57c0cea83a66f3f3665d50d
KMGIKK <86357105+KMGIKK@users.noreply.github.com>
2023-10-22 14:30:39 +0530

    add analisis file

--- src/com/devstack/pos/db/DbConnection.java
+++ src/com/devstack/pos/db/DbConnection.java
@@ -12,7 +12,7 @@ public class DbConnection {
     private DbConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka",
-                "root","1234");
+                "root","");
     }
     public static DbConnection getInstance() throws SQLException, ClassNotFoundException {
         return (dbConnection==null)?dbConnection= new DbConnection():dbConnection;
